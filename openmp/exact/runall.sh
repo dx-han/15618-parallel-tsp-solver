be94c02692e599623ed845358bcbdbd8dafa738f
dongxiah <dongxiah@andrew.cmu.edu>
2022-05-01 13:35:21 -0400

    experiment result

--- openmp/exact/runall.sh
+++ openmp/exact/runall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PREFIX="../../inputs/"
-FILES=("input_20_100x100.txt")
+FILES=("TSP_input_10.txt" "TSP_input_15.txt" "TSP_input_20.txt")
 PROGRAM="./exactopenmp"
-THREADS=(1 8 16 32 64 128)
+THREADS=(1 8 16 32 64)
 for FILE in ${FILES[@]}
 do
     for THREAD in ${THREADS[@]}
